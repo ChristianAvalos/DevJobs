85edf7b3438ad679c19d1cff7dcb5577295830e9
Christian <christiand.avalos.e@gmail.com>
2023-05-04 00:04:16 -0400

    mejorando el login y el dashboard

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        VerifyEmail::toMailUsing(function($notifiable,$url){
+            return (new MailMessage)
+            ->subject('Verificar cuenta')
+            ->line('Tu cuenta ya esta casi lista, solo debes presionar el enlace a continuaciòn')
+            ->action('Confirmar cuenta',$url)
+            ->line('Si no creaste esta cuenta, puedes ignorar este mensaje');
+        });
     }
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,13 +8,28 @@
             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
-
+            
         <!-- Email Address -->
         <div class="mt-4">
             <x-input-label for="email" :value="__('Email')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
+
+        <!-- Rol -->
+        <div class="mt-4">
+            <x-input-label for="rol" :value="__('¿Que tipo de cuenta deseas en DevJobs?')" />
+            <select 
+            name="rol" 
+            id="rol"
+            class ='border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm w-full'
+            >
+                <option value="">-- Selecciona un rol --</option>
+                <option value="1">Developer - Obtener empleo</option>
+                <option value="2">Recruiter - Publicar empleos</option>
+            </select>
+            <x-input-error :messages="$errors->get('rol')" class="mt-2" />
+            </div>
 
         <!-- Password -->
         <div class="mt-4">
