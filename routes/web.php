c36c33b7b345903eb7418dbd385acdf657f83f1a
Christian <christiand.avalos.e@gmail.com>
2023-05-08 22:36:16 -0400

    Mejorar vista de dashboard devjobs

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\VacanteController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,10 +19,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
+Route::get('/dashboard',[VacanteController::class,'index'] )->middleware(['auth', 'verified'])->name('vacantes.index');
+Route::get('/vacantes/create',[VacanteController::class,'create'] )->middleware(['auth', 'verified'])->name('vacantes.create');
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
