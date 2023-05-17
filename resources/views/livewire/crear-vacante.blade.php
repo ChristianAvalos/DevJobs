3b70ba71b7368207129112d814ad39c276361ffa
Christian <christiand.avalos.e@gmail.com>
2023-05-16 21:57:47 -0400

    creando migraciones

--- resources/views/livewire/crear-vacante.blade.php
+++ resources/views/livewire/crear-vacante.blade.php
@@ -81,6 +81,14 @@
         <x-text-input id="imagen" class="block mt-1 w-full" type="file" 
         accept="image/*"
         wire:model="imagen" />
+        <div class="my-5 w-80">
+            @if ($imagen)
+                Imagen:
+                <img src="{{$imagen->temporaryUrl()}}"/>
+                
+            @endif
+
+        </div>
         @error('imagen')
             <livewire:mostrar-alerta :message="$message"/>   
         @enderror
