041d09ac8cf0a56ce61479039514327c4a8fecdd
Christian <christiand.avalos.e@gmail.com>
2023-06-28 22:01:27 -0400

    puliendo notificaciones

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -10,16 +10,17 @@
                     </a>
                 </div>
                 @auth
-                    <!-- Navigation Links -->
-                    <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                        <x-nav-link :href="route('vacantes.index')" :active="request()->routeIs('vacantes.index')">
-                            {{ __('Mis vacantes') }}
-                        </x-nav-link>
-                        <x-nav-link :href="route('vacantes.create')" :active="request()->routeIs('vacantes.create')">
-                            {{ __('Crear vacante') }}
-                        </x-nav-link>
-                    </div>
-
+                    @can('create', App\Model\Vacante::class)
+                        <!-- Navigation Links -->
+                        <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                            <x-nav-link :href="route('vacantes.index')" :active="request()->routeIs('vacantes.index')">
+                                {{ __('Mis vacantes') }}
+                            </x-nav-link>
+                            <x-nav-link :href="route('vacantes.create')" :active="request()->routeIs('vacantes.create')">
+                                {{ __('Crear vacante') }}
+                            </x-nav-link>
+                        </div>
+                    @endcan
                 @endauth
             </div>
 
@@ -27,13 +28,13 @@
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ml-6">
                 @auth
-                    @if (auth()->user()->rol === 2)
+                    @can('create', App\Model\Vacante::class)
                         <a  class="mr-2 w-7 h-7 bg-indigo-600 hover:bg-indigo-800 rounded-full 
                         flex flex-col justify-center items-center text-sm font-extrabold text-white" href="{{route('notificaciones')}}">
                             {{ Auth::user()->unreadNotifications->count()}}
                         </a>
                         
-                    @endif
+                    @endcan
                     <x-dropdown align="right" width="48">
                         <x-slot name="trigger">
                             <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
@@ -104,6 +105,22 @@
                     {{ __('Crear vacantes') }}
                 </x-responsive-nav-link>
             </div>
+            @if (auth()->user()->rol === 2)
+                <div class="flex gap-2 items-center p-3">
+                    <a class="w-7 h-7 bg-indigo-600 hover:bg-indigo-800 rounded-full 
+                    flex flex-col justify-center items-center text-sm font-extrabold text-white"
+                        href="{{route('notificaciones')}}">
+                        {{ Auth::user()->unreadNotifications->count()}}
+                    </a>
+                    <p class="text-base font-medium text-gray-600">
+                        
+                        @choice('Notificacion|Notificaciones', Auth::user()->unreadNotifications->count())
+                    
+                    </p>
+                </div>
+
+            
+            @endif
 
             <!-- Responsive Settings Options -->
             <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
